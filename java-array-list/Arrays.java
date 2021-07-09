914f36ee91bc44c463d8c19da5392339ba3bb531
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-07-09 16:34:57 -0700

    Add files via upload

---
+++ java-array-list/Arrays.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Arrays {
+  public static void main(String[] args) {
+    ArrayList<String> names = new ArrayList<String>();
+    names.add("Sam");
+    names.add("Shawna");
+    names.add("Brianna");
+
+    for(String qweqweqwe : names) {
+      System.out.println(qweqweqwe);
+    }
+  }
+}
