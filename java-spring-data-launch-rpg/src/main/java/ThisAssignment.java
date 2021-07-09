5a75c5281fe0c30051972f4022910aff59ad171c
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-07-09 15:57:50 -0700

    data launch rpg

---
+++ java-spring-data-launch-rpg/src/main/java/ThisAssignment.java
@@ -0,0 +1,11 @@
+public class ThisAssignment {
+
+    public static void main(String[] args) {
+        System.out.println(needToSubmitSomething());
+    }
+    public static String needToSubmitSomething(){
+        return "there were too many assignments this week and half of them live in the '" +
+                "wrong folder so I'm turning this in since it is supposed to be the one " +
+                "from the reading but isn't for some reason. Too many assignments this week."
+    }
+}
