9bffb78c17a50d61da112b842b0c3c198f8aabec
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-07-09 16:10:04 -0700

    leetcode

---
+++ leetCode/src/leetCode.java
@@ -0,0 +1,18 @@
+public class leetCode {
+//TWO SUM
+//public int[] twoSum(int[] nums, int target) {
+//    int[] thisArray = new int[2];
+//    for(int i = 0; i < nums.length; i++){
+//        for(int j=0; j < nums.length; j++) {
+//            int thisValue = nums[i] + nums[j];
+//            if(thisValue == target)
+//            {
+//                thisArray[0] = nums[i];
+//                thisArray[1] = nums[j];
+//            }
+//        }
+//    }
+//    return thisArray;
+//}
+
+}
