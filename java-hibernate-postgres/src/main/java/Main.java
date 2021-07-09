c3b4a34db45098ba9183064abc283231449feb99
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-07-09 16:59:49 -0700

    Add files via upload

---
+++ java-hibernate-postgres/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+  public static void main(String[] args) {
+    EntityManagerFactory emf =
+        Persistence.createEntityManagerFactory("com.launchacademy.books");
+    EntityManager em = emf.createEntityManager();
+
+    try {
+      // our database interactions can go here
+    }
+    finally {
+      em.close();
+      emf.close();
+    }
+  }
+}
