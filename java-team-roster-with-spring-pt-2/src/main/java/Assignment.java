be4a84d732d7b6f8e970efb116e3432c8be3aca7
Karalynn Partain <78173292+kpartain@users.noreply.github.com>
2021-07-09 15:58:21 -0700

    java team roster part 2

---
+++ java-team-roster-with-spring-pt-2/src/main/java/Assignment.java
@@ -0,0 +1,8 @@
+public class Assignment {
+    public static void main(String[] args) {
+        System.out.println(thisAssignment());
+    }
+    public static String thisAssignment(){
+        return "is in another folder so I'm submitting this"
+    }
+}
